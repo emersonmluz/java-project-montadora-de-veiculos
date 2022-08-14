60f67d8ce9f6d2116aa6be86384f3c8532163dcf
Émerson Luz <emersonmluz@gmail.com>
2022-08-14 17:20:11 -0300

    Alterado código para obter mehor performance e visual limpo.

--- file/Main.java
+++ file/Main.java
@@ -66,7 +66,7 @@ class Main {
 						}
 					}
 
-					if (vazio == true) {
+					if (vazio) {
 						System.out.println();
 						System.out.println("Nenhum registro foi encontrado.");
 						System.out.println();
